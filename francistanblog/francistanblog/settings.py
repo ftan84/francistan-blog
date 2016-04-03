6698f5f3ec5af40228839b286974cf0c318ac355
Francis Tan <francis.adrian.tan@gmail.com>
2016-04-02 21:56:17 -0700

    Started new Django project

--- francistanblog/francistanblog/settings.py
+++ francistanblog/francistanblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9onpt3zyp@n#r)4#0ro#j@tnk^)!dckgn-m1f$75k*(b-a)b2_'
+SECRET_KEY = 'hqbo-fy&hpgyx%+f_%580ni5(y%)mt6gv@**7@5_lpp@nb=5$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
