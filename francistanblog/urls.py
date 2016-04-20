9ac707bc4debf06049575c949a862f816c4ee9a2
Francis Tan <francis.adrian.tan@gmail.com>
2016-04-19 18:53:45 -0700

    Changed app from posts to blog

--- francistanblog/urls.py
+++ francistanblog/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^posts/', include('posts.urls')),
+    url(r'^$', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
 ]
