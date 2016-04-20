841e600e06c68b34289ab3b58e9b9f44b4943bf4
Francis Tan <francis.adrian.tan@gmail.com>
2016-04-19 17:52:37 -0700

    Started to build out posts model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Post Model
+
+    This is the model that represents an instance of a blog post.
+    """
+    title = models.CharField(max_length=200)
+    author
+    status
+    created_date
+    published_date
+    body
