3d0db3c8e71082f1273c4fd84b7647416c986ff2
Francis Tan <francis.adrian.tan@gmail.com>
2016-04-24 16:54:39 -0700

    Started new project using djangocms

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-
-app_name = 'blog'
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<slug>.+)/$', views.detail, name='detail')
-]
